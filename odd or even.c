00aacd30f15cb537e01b5406f3f2e37d9746d55a
01fe22bec233 <130544297+01fe22bec233@users.noreply.github.com>
2024-03-11 22:45:36 +0530

    Add files via upload

---
+++ odd or even.c
@@ -0,0 +1,17 @@
+/*program to compute number is odd or even*/
+#include<stdio.h>
+int main()
+{
+   int number;
+   printf("enter the number\n");
+   scanf("%d",&number);
+   if(number%2==0)
+   {
+      printf("the number is even");
+   }
+   else
+   {
+      printf("the number is odd");
+   }
+   return 0;
+   }
